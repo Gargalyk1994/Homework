8cce51ab5d0d73825bef35f6ace0bbf2cf32ddeb
Anton Gargalyk <gargalyk1994@gmail.com>
2023-02-14 19:36:39 +0900

    change task 4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,6 +1,15 @@
-﻿int a = 2;
-int b = 3;
-int c = 7;
+﻿//Напишите программу, которая принимает
+// на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Enter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third number: ");
+int c = int.Parse(Console.ReadLine());
 
 int max = a;
 
