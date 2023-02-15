4c6df73f4ffab23e2ac40f9cb37a409b887f0ce9
Anton Gargalyk <gargalyk1994@gmail.com>
2023-02-15 18:40:03 +0900

    task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+int size = 8;
+int index = 0;
+
+while (index < size)
+{
+    if (array[index] % 2 == 0)
+    {
+       Console.Write(array[index]); 
+    }
+    else if (array[index] % 2 == 1)
+    {
+        Console.Write(",");
+    }
+    index= index + 1;
+}
+
